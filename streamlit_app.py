cacf8258ee9d785a10848cd63ed850270e449fc6
Swaroop N Neti <swaroop.neti@gmail.com>
2025-11-30 20:51:48 -0500

    Update seach on api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if ingredients_list:
         st.write('The search value for ', each_fruit, 'is ', search_on, '.')
         st.subheader(each_fruit + ' Nuitrition Information')
         
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + each_fruit)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
