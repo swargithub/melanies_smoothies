9a47e94037acb6e771e6a74972ea8e0ed7b899e2
Swaroop N Neti <swaroop.neti@gmail.com>
2025-11-30 20:46:50 -0500

    search on pd df

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,9 @@ if ingredients_list:
 
     for each_fruit in ingredients_list:
         ingredients_string += each_fruit + ' '
+
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, 'is ', search_on, '.')
         st.subheader(each_fruit + ' Nuitrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + each_fruit)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
